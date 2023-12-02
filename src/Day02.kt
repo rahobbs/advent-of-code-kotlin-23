df0dc7ca83e9b88530c1eb8043468ce1dffd89dd
Rachael Hobbs <hobbs.rachael@gmail.com>
2023-12-02 14:54:22 -0500

    solution for part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,12 +5,12 @@ fun main() {
     fun part1(input: List<String>): Int {
         var sum = 0
 
-        input.forEach breaking@ { game ->
+        input.forEach breaking@{ game ->
             val parts = game.split(":")
             val gameId = parts.first().removePrefix("Game ").toInt()
 
             val draws = parts.last().split(';')
-            draws.forEach{ draw ->
+            draws.forEach { draw ->
                 val colors = draw.split(',')
                 colors.forEach { color ->
                     val pair = color.trimStart().split(" ")
@@ -30,12 +30,40 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+
+        input.forEach { game ->
+            val draws = game.split(":").last().split(";")
+
+            val colorMap = mutableMapOf("red" to 0, "blue" to 0, "green" to 0)
+
+            draws.forEach { draw ->
+                val colors = draw.split(",")
+
+                colors.forEach { colorFreq ->
+                    val pair = colorFreq.trimStart().split(" ")
+                    val color = pair.last()
+                    val number = pair.first().toInt()
+
+                    if (colorMap[color]!! < number) {
+                        colorMap[color] = number
+                    }
+                }
+            }
+
+            val power = colorMap.values.reduce { acc, i ->
+                acc * i
+            }
+
+            sum += power
+
+        }
+        return sum
     }
 
 // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day02_test")
-//    val result = part1(testInput)
+    val testInput = readInput("Day02_test")
+    // val result = part2(testInput)
     //check(part1(testInput) == 1)
 
     val input = readInput("Day02")
