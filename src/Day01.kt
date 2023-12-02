987edc4b5239cc84b1b32bf627923b71b2fe32ec
Rachael Hobbs <hobbs.rachael@gmail.com>
2023-12-01 21:10:54 -0500

    solutions for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,102 @@
+import java.lang.StringBuilder
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach {str ->
+            var sb = StringBuilder()
+
+            run breaking@ {
+                str.forEach {
+                    if(it.isDigit()) {
+                        sb.append(it)
+                        return@breaking
+                    }
+                }
+            }
+
+            for (i in str.length - 1 downTo 0) {
+                if (str[i].isDigit()) {
+                    sb.append(str[i])
+                    break
+                }
+            }
+            sum += sb.toString().toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val numTrie = Trie<Int>()
+        numTrie.insert("one", 1)
+        numTrie.insert("two", 2)
+        numTrie.insert("three", 3)
+        numTrie.insert("four", 4)
+        numTrie.insert("five", 5)
+        numTrie.insert("six", 6)
+        numTrie.insert("seven", 7)
+        numTrie.insert("eight", 8)
+        numTrie.insert("nine", 9)
+
+        val reverseTrie = Trie<Int>()
+        reverseTrie.insert("eno", 1)
+        reverseTrie.insert("owt", 2)
+        reverseTrie.insert("eerht", 3)
+        reverseTrie.insert("ruof", 4)
+        reverseTrie.insert("evif", 5)
+        reverseTrie.insert("xis", 6)
+        reverseTrie.insert("neves", 7)
+        reverseTrie.insert("thgie", 8)
+        reverseTrie.insert("enin", 9)
+
+        var sum = 0
+
+        input.forEach { str ->
+            val sb = StringBuilder()
+
+            run breaking@ {
+                str.forEachIndexed{i, ch ->
+                    if (ch.isDigit()) {
+                        sb.append(ch)
+                        return@breaking
+                    } else {
+                        val substring = str.substring(i, str.length -1)
+                        val searchResult = numTrie.contains(substring)
+                        if (searchResult != null) {
+                            sb.append(searchResult.toString())
+                            return@breaking
+                        }
+                    }
+                }
+            }
+
+            val reversed = str.reversed()
+
+            run breaking@ {
+                reversed.forEachIndexed{i, ch ->
+                    if (ch.isDigit()) {
+                        sb.append(ch)
+                        return@breaking
+                    } else {
+                        val substring = reversed.substring(i, reversed.length -1)
+                        val searchResult = reverseTrie.contains(substring)
+                        if (searchResult != null) {
+                            sb.append(searchResult.toString())
+                            return@breaking
+                        }
+                    }
+                }
+            }
+            sum += sb.toString().toInt()
+
+        }
+
+        return sum
+    }
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -12,10 +12,63 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Implements a Trie with terminal points indicated by the
+ * existence of a Value
+ */
+
+class Trie<Value> {
+
+    val root = Node<Value>()
+
+    data class Node<Value>(
+            val children: MutableMap<Char, Node<Value>> = mutableMapOf(),
+            var value: Value? = null,
+    )
+
+    fun insert(key: String, value: Value) {
+        var currentNode = root
+        for (char in key) {
+            if (currentNode.children[char] == null) {
+                currentNode.children[char] = Trie.Node()
+            }
+            currentNode = currentNode.children[char]!!
+        }
+        currentNode.value = value
+    }
+
+    fun search(key: String): Value? {
+        var currentNode = root
+        for (char in key) {
+            if (currentNode.children[char] == null) {
+                return null
+            }
+            currentNode = currentNode.children[char]!!
+        }
+        return currentNode.value
+    }
+
+    fun contains(key: String): Value? {
+        var currentNode = root
+        for (char in key) {
+
+            if (currentNode.children[char] == null) {
+                return null
+            }
+            currentNode = currentNode.children[char]!!
+
+            if (currentNode.value != null) {
+                return currentNode.value
+            }
+        }
+        return currentNode.value
+    }
+}
